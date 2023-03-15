2f2837a4a797d5d75a28cfd8e678c067ac2d23da
Joshua Suntup <joshuasuntup@Joshuas-MacBook-Pro.local>
2023-03-15 11:46:35 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [values, setValues] = useState('');
+  const [target, setTarget] = useState('');
+
+  const handleValuesChange = (event) => {
+    setValues(event.target.value);
+  };
+
+  const handleTargetChange = (event) => {
+    setTarget(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Send the user input to the backend server and update the result
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Find Combination</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Values:
+          <input type="text" value={values} onChange={handleValuesChange} />
+        </label>
+        <br />
+        <label>
+          Target:
+          <input type="text" value={target} onChange={handleTargetChange} />
+        </label>
+        <br />
+        <button type="submit">Find Combination</button>
+      </form>
+      {/* TODO: Display the result of the calculation */}
     </div>
   );
 }
