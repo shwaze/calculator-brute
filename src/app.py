79cb4627d8a78a2978bafb77a655aa3b427b8eef
shwaze <39320265+shwaze@users.noreply.github.com>
2023-03-15 11:49:43 +1100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from flask import Flask, request, jsonify
+from itertools import combinations
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app) #comment this on deployment
-api = Api(app)
+app = Flask(__name__)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+@app.route('/', methods=['POST'])
+def find_combination():
+    data = request.get_json()
+    values = data['values']
+    target = int(data['target'])
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+    # Use itertools to generate all possible combinations of values
+    # that can be added together to equal the target value
+    combos = []
+    for i in range(len(values)):
+        for c in combinations(values, i+1):
+            if sum(c) == target:
+                combos.append(c)
+
+    # Return the results as a JSON object
+    return jsonify(combos)
+
+if __name__ == '__main__':
+    app.run()
